79110c00e7dae67c1b31042d1ca93af36cd9eae7
nasergamal <naser.gamal98@gmail.com>
2023-02-16 15:36:31 -0800

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: time to show my genius
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+char *stre ="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+printf("%s\n", stre);
+
+return (1);
+}
