893937abc6fc677d8690bfb8f3a1a47d8430918d
nasergamal <naser.gamal98@gmail.com>
2023-02-21 10:51:44 -0800

    first two tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing char
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_alphabet - Print letters in lowercase
+ *
+ * Description: print all the alphabets
+ *
+ * Return: Always 0 (success)
+ */
+
+int print_alphabet(void)
+{
+char i
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+
+return (0);
+}
