a54640a5372ee2f442667a4aa501e6e8a8517cef
nasergamal <naser.gamal98@gmail.com>
2023-02-21 11:43:20 -0800

    3rd

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main."
+/**
+ * _islower - determine letters case
+ * @c: the letter to be identified
+ *
+ * Description: find out the truth about the letter
+ *
+ * Return: true or false
+ */
+
+int _islower(int c)
+{
+int r;
+if (islowerr(c))
+r = 1;
+else
+r = 0;
+
+return (r);
+}
