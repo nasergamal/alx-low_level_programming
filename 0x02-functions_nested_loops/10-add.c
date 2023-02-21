5994a8ebb60e82ceb00763e2a868abe6b05b1d50
nasergamal <naser.gamal98@gmail.com>
2023-02-21 15:08:48 -0800

    10-

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - just add thing
+ *
+ * Description: really it only adds things
+ *
+ * return: addition result
+ */
+
+int add(int n, int m)
+{
+int b;
+
+b =  n + m;
+
+return (b);
+}
