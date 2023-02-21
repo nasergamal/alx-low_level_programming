3546d53909c9f64962c246bf0153dc06388416b1
nasergamal <naser.gamal98@gmail.com>
2023-02-21 12:12:53 -0800

    2 more

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_sign - detect negativity
+ * @n: the number to be identified
+ *
+ * Description: find out the truth about numbers
+ *
+ * Return: numbers based on negativity
+ */
+
+int print_sign(int n)
+{
+int r;
+
+if (n > 0)
+r = 1;
+
+else if (n < 0)
+r = -1;
+
+else
+r = 0;
+
+return (r);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - determine absolute value
+ * @int: the number under investigation
+ *
+ * Description: find the absolute truth about the number
+ *
+ * Return: Truth
+ */
+
+int _abs(int)
+{
+int r;
+
+r = abs(int* int);
+
+return (r);
+}
