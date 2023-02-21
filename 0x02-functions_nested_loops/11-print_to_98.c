026e0aafdb4159803b0a1a9fc995ab6a031a5e87
nasergamal <naser.gamal98@gmail.com>
2023-02-21 15:43:47 -0800

    11-

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - just add thing
+ * @n: the start of the count
+ *
+ * Description: just counting
+ *
+ * Return: count to ninty eight
+ */
+
+int print_to_98(int n)
+{
+int m, v;
+if (n < 98)
+{
+for (; n < 98; n++)
+{
+if (n > 10 && n < -10)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+}
+else
+{
+putchar(n + '0');
+}}}
+else if (n > 98)
+{
+for (; n > 98; n--)
+{
+if (n >= 100)
+{
+v = n % 10;
+n = n / 10;
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+putchar(v + '0');
+}
+else if (n < 100 && n > 98)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+}
+putchar(',');
+putchar(' ');
+}
+if (n == 98)
+putchar(n / 10);
+putchar(n % 10);
+putchar('\n');
+}
+return (0); }
