169f01f68397a6adcdf31c65a3d1623b0b15e4e7
nasergamal <naser.gamal98@gmail.com>
2023-02-22 13:10:57 -0800

    last adv task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,22 +9,37 @@
 
 int main(void)
 {
-long int x, y, z;
-int s;
-x = 1, y = 2, z = 2, s = 0;
+unsigned long int x, y, z, x1, x2, y1, y2, z1, z2;
+int s, n;
+x = 1, y = 2, z = 2, s = 0, n = 100000000;
 
-while (s < 49)
+while (s < 45)
 {
-if (s < 48)
 printf("%lu, %lu, ", x, y);
-
-else 
-printf("%lu, %lu\n", x, y);
-
 z = x + y;
 x = z;
 y += z;
+s++;
+}
+x1 = x / n;
+x2 = x % n;
+y1 = y / n;
+y2 = y % n;
+z1 = z / n;
+z2 = z % n;
 
+while (s < 49)
+{
+printf(", %lu%lu", x1, x2);
+printf(", %lu%lu", y1, y2);
+
+
+z1 = x1 + y1;
+z2 = x2 + y2;
+x1 = z1;
+x2 = z2;
+y1 += z1;
+y2 += z2;
 s++;
 }
 return (0);
