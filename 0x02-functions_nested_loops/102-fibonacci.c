860512ece244835c478a5272252971513689d6c4
nasergamal <naser.gamal98@gmail.com>
2023-02-21 17:43:31 -0800

    3rd (forget about the 2nd

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: fi
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int x, y, z, i;
+x = 1, y = 2;
+z = x + y;
+
+for (i = 0; i < 50; i++
+printf("%d, %d,", x, y);
+x = y;
+y = z;
+
+return (0);
+}
