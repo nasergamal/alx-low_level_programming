b78fdd753fb3dfd16478b1d5f824a231d03070dc
nasergamal <naser.gamal98@gmail.com>
2023-02-21 17:17:02 -0800

    1st adv

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - make your choice
+ * @n: the number
+ *
+ * Description: timetable
+ *
+ * Return: your choice
+ */
+void print_times_table(int n);
+int m, v, d;
+
+for (m = 0; m <= n; m++)
+{
+for (v = 0; v <= n; v++)
+{
+d = m * v;
+if (v == 0)
+{
+_putchar(d + '0');
+_putchar(',')
+}
+elseif(d < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(d + '0');
+}
+elseif(d > 10 && d < 100)
+{
+_putchar(' ');
+_putchar(d / 10 + '0');
+_putchar(d % 10 + '0');
+
+}
+elseif(d >= 100)
+{
+_putchar(d / 100 + '0');
+_putchar((d / 10) % 10 + '0');
+_putchar(d % 10 + '0');
+}
+if (v < n)
+{
+_putchar(' ');
+_putchar(';');
+}
+_putchar('\n');
+}
