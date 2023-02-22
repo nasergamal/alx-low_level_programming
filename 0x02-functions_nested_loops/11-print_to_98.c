820f272de816f345d83428d21a80bd9ab891b8b6
nasergamal <naser.gamal98@gmail.com>
2023-02-21 16:31:42 -0800

    11-

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,13 +13,16 @@
 void print_to_98(int n)
 {int m, v;
 if (n < 98)
-{for (; n < 98; n++)
+{for (; n <= 98; n++)
 if (n < 0)
 {m = abs(n);
 _putchar('-'); }
 if (n >= 10 || n <= -10)
 {putchar(m / 10 + '0');
 putchar(m % 10 + '0');
+if (n == 98)
+{_putchar('\n');
+continue; }
 putchar(',');
 putchar(' '); }
 else
@@ -36,11 +39,10 @@ _putchar(m % 10 + '0');
 _putchar(v + '0');
 _putchar(',');
 _putchar(' '); }
-else if (n < 100 || n > 98)
+else if (n < 100 || n >= 98)
 {
 _putchar(n / 10 + '0');
-_putchar(n % 10 + '0'); }}}
-if (n == 98)
-{_putchar(n / 10 + '0');
 _putchar(n % 10 + '0');
-_putchar('\n'); }}
+if (n == 98)
+_putchar('\n');
+continue; }}}}}
