63db8095581ef4d0e5790d6226d412e60da6fd6a
nasergamal <naser.gamal98@gmail.com>
2023-02-23 14:41:30 -0800

    1st adv

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: get the biggest prime number
+ *
+ * Return: the biggest value
+ */
+
+int main(void)
+{
+	long int i = 612852475143, m;
+
+	int c = 0;
+
+	for (m = 1; m < i; m += 2)
+	{
+		if (i % m == 0)
+			c = m;
+	}
+	printf("%lu", c);
+	return (0);
+}
