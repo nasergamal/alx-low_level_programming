29757fb6d8e70c80f6a3e58033c989a7db7f9914
nasergamal <naser.gamal98@gmail.com>
2023-02-27 15:29:59 -0800

    1st adv

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - Entry point
+ * @s: the string to be converted
+ *
+ * Description: take a string and convert it to integer
+ *
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int n;
+
+	n = atoi(s);
+
+	return (n);
+}	
