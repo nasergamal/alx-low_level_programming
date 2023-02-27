c43adb50b28d409520f7d461f4d891228e43849c
nasergamal <naser.gamal98@gmail.com>
2023-02-27 10:49:39 -0800

    1st mandatory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes the variable to be changed
+ * @n: the int to be changed
+ *
+ * Description: takes a pointer to an int and changed the int to another value
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int *p = &n;
+
+	*p = 98;
+}
