4641e493dba8a40d0c82a896ce98a426241cef31
nasergamal <naser.gamal98@gmail.com>
2023-03-01 15:34:58 -0800

    3-

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, s = 0;
+	int i;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[0] != s2[0])
 		{
-			s = s1[0] - s2[0];
-			return (s);
+			return (s1[0] - s2[0]);
 		}
 	}
-	return (s);
+	return (0);
 }
