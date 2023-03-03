5aa55e51e2d21f2d51e88c0ad83e5296cac3dc05
nasergamal <naser.gamal98@gmail.com>
2023-03-02 18:36:48 -0800

    last adv

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_buffer - print buffers
+ * @b: the array to be printed
+ * @size: arrays size
+ *
+ * Description: takes an array and print it in 10 bytes per line form
+ *
+ * Return nothing
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, n, m;
+
+	for (i = 0; i < size;)
+	{
+		printf("%p:", &b[i]);
+		for (n = 0; n < 10; n += 2, i += 2)
+		{
+			printf(" %02x%02x ", b[i], b[i + 1]);
+		}
+		for (m = 0, i -= 10; m < 10; m++, i++)
+			printf("%c", b[i]);
+		putchar('\n');
+	}
+}
