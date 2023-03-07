c92387df9ce56aece07ed951a932e3e74a507876
nasergamal <naser.gamal98@gmail.com>
2023-03-06 17:25:40 -0800

    5-

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,18 +13,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j = 0, m = 0, n = 0, c = 0;
+	int i, j;
 
-	for (i = 0;; i++)
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (; needle[j] != '\0'; j++)
+		if ( haystack[i] == needle[j])
 		{
-			if (needle[j] != haystack[j + i])
-				break;
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (needle[j] != haystack[i + j])
+					break;
 		}
 		if (!needle[j])
 			return (&haystack[i]);
-		if (haystack[i] == '\0')
-			return (NULL);
+		}
 	}
+	return (NULL);
 }
