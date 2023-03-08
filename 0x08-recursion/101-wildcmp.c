3157eb98ba7c3afc505b22502fc47dd4efa12fa3
nasergamal <naser.gamal98@gmail.com>
2023-03-08 08:32:03 -0800

    2nd adv

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+
+/**
+ * wildcmp - takes two strings to compare
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ *
+ * Return: 1 or 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s2 == '*' && *(s2 + 1) == '*')
+		return (wildcmp(s1, s2 += 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 += 1, s2 += 1));
+	if (*s2 == '*')
+		return (wildcmp(s1 += 1, s2))
+			|| (wildcmp(s1, s2 += 1));
+	return (0);
+}
