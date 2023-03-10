8581b262998292f787b50095667bdfb1f042d104
nasergamal <naser.gamal98@gmail.com>
2023-03-10 06:52:49 -0800

    (0- 1- unused) 4- changed digit verification)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
  * main - Entry point
  * @argc: arguments count
@@ -13,7 +15,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		if (!(isdigit(*argv[i])))
 		{
 			printf("Error\n");
 			return (1);
