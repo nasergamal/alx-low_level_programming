3274efcc0a0c5245e697f2a64887e9a04a469718
nasergamal <naser.gamal98@gmail.com>
2023-03-13 13:02:36 -0700

    1st adv

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int len(char *c);
+
+/**
+ * argstostr - concatenate any number of strings and return pointer to mem loc
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: pointer to memory
+ */
+
+
+char *argstostr(int ac, char **av)
+{
+	int i, n = 0, m, v = 0;
+
+	char *s, *b;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+		n += len(av[i]);
+	s = malloc((n + ac - 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+	{
+		b = av[i];
+		for (m = 0; b[m] != '\0'; m++)
+			s[m + v] = b[m];
+		s[m + v] = '\n';
+		v += m + 1;
+	}
+	return (s);
+}
+
+/**
+ * len - measure string length
+ * @c: the string
+ *
+ * Return: length
+ */
+
+int len(char *c)
+{
+	int a;
+
+	for (a = 0; c[a] != '\0'; a++)
+		;
+	return (a);
+}
