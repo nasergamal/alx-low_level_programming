d285818237e505ba9267080584bab4ede86e450e
nasergamal <naser.gamal98@gmail.com>
2023-03-14 07:41:11 -0700

    101-

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,7 @@ char **strtow(char *str)
 	s = malloc((n + 1) * sizeof(char *));
 	if (s == NULL)
 		return (s);
-	for (i = 0; i <= len(str); i++)
+	for (i = 0; str[i] ; i++)
 	{
 		if (str[i] != ' ' && str[i] != '\0')
 			wo++;
