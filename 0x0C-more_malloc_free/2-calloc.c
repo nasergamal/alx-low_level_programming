6304e325397fb454bf3d30d66a2131d91b672d4e
nasergamal <naser.gamal98@gmail.com>
2023-03-16 05:27:55 -0700

    2-

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,6 +14,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	s = malloc(nmemb * size);
+	s = malloc((nmemb + 1) * size);
 	return (s);
 }
