75952eeb5b01c58e3c27e5804a2447c5a97bf841
nasergamal <naser.gamal98@gmail.com>
2023-03-16 06:45:26 -0700

    1st adv

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+void me(char *s, void *p, unsigned int o, unsigned int n);
+
+/**
+ * _realloc - reallocate memory based on requirement
+ * @ptr: pointer to already allocated memory
+ * @old_size: the size of ptr
+ * @new_size: the new size to be allocate
+ *
+ * Return: pointer to new memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *s;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	s = malloc(new_size);
+	if (s == NULL || ptr == NULL)
+		return (s);
+	s = ptr;
+	free(ptr);
+	return (s);
+}
