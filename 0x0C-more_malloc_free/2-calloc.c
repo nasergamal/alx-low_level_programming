78a81442506fede2aeba54bdb45dfd8dc732ea31
nasergamal <naser.gamal98@gmail.com>
2023-03-16 05:57:34 -0700

    2-

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+char *me(char *s, unsigned int n);
 /**
  * _calloc - allocate memory set to zero
  * @nmemb: number of elements
@@ -12,17 +12,27 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
 
-	char *c;
-
-	unsigned int i;
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	s = malloc((nmemb + 1) * size);
+	s = malloc((nmemb * size);
 	if (s == NULL)
-		return (s);
-	c = s;
-	for (i = 0; i <= nmemb; i++)
-		c[i] = 0;
+		return (NULL);
+	me(s, nmemb);
+	return (s);
+}
+
+/**
+ * me - set memory to 0
+ * @s: pointer
+ * @n: size
+ *
+ * Return: pointer
+ */
+char *me(char *s, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = 0;
 	return (s);
 }
