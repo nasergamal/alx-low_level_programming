c648161f1395a1d9b520f827eb22a70a328fc25c
nasergamal <naser.gamal98@gmail.com>
2023-03-16 05:23:41 -0700

    2-

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
-	if (s == NULL) /* Redundant remove later*/
-		return (s);
 	return (s);
 }
