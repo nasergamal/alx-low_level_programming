4dfe332a78dc3e58f084cfc8174abeb4ab7f7833
nasergamal <naser.gamal98@gmail.com>
2023-03-16 05:42:41 -0700

    2-

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,8 +12,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
 
+	char *c;
+
+	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc((nmemb + 1) * size);
+	if (s == NULL)
+		return (s);
+	c = s;
+	for (i = 0; i <= nmemb; i++)
+		c[i] = 0;
 	return (s);
 }
