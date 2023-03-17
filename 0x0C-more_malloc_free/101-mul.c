b604cd8fdc69e48e60c0a00d93bd5bf1113b483c
nasergamal <naser.gamal98@gmail.com>
2023-03-16 19:10:41 -0700

    place holder

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * main - Entry
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: hope
+ */
+void main(void)
+{
+}
