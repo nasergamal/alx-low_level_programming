72ef780410b08ddd3f2ecb0997a8af9296a0cdd0
nasergamal <naser.gamal98@gmail.com>
2023-03-20 11:17:23 -0700

    4-

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,6 +16,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	int i, n, m;
 
+	if (!(name) || !(owner))
+		return (NULL);
 	for (n = 0; name[n] != '\0'; n++)
 		;
 	for (m = 0; owner[m] != '\0'; m++)
