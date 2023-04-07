c96bf6bf210b0dfbc08c57fd7d127f83b16f4631
nasergamal <naser.gamal98@gmail.com>
2023-04-07 23:26:53 +0200

    3- error msg fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+void close_fd(int fd);
 /**
  * main - copy contnent of file 1 to file 2
  * @ac: number of argument
@@ -10,7 +10,7 @@
 
 int main(int ac, char *av[])
 {
-	int fp1, fp2, n, f1, f2;
+	int fp1, fp2, n, f2;
 	char *buf;
 
 	if (ac != 3)
@@ -18,36 +18,52 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fp1 = open(av[1], O_RDONLY);
 	buf = malloc(sizeof(char) * 1024);
 	if (buf == NULL)
 	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", av[2]);
 		exit(99);
 	}
+	fp1 = open(av[1], O_RDONLY);
 	n = read(fp1, buf, 1024);
 	fp2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
 		if (fp1 == -1 || n == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			free(buf);
 			exit(98);
 		}
 		f2 = write(fp2, buf, n);
 
 		if ((fp2 == -1) || f2 == -1)
 		{
-			dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			free(buf);
 			exit(99);
 		}
 		n = read(fp1, buf, 1024);
 		fp2 = open(av[2], O_WRONLY | O_APPEND);
 	} while (n > 0);
-	f1 = close(fp1);
-	f2 = close(fp2);
-	if (f1 == -1 || f2 == -1)
+	close_fd(fp1);
+	close_fd(fp2);
+	return (0);
+}
+
+/**
+ * close_fd - close fd
+ * @fd: to be closed
+ *
+ * Return: void
+ */
+void close_fd(int fd)
+{
+	int f1;
+	
+	f1 = close(fd);
+	if (f1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (f1 == -1 ? fp1 : fp2));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
-	} return (0);
+        }
 }
