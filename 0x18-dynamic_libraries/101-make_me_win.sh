6745cde86a7f3fa2fa58a88f91956028720f3b2e
nasergamal <naser.gamal98@gmail.com>
2023-05-22 16:34:43 +0200

    101-

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/nasergamal/alx-low_level_programming/raw/main/0x18-dynamic_libraries/n.so
+wget -P /tmp https://github.com/nasergamal/alx-low_level_programming/raw/master/0x18-dynamic_libraries/n.so
 export LD_PRELOAD=tmp/n.so
