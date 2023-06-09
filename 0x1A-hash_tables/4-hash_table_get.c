5ec81fa56c26547334fabdbc85177b72b4d3040a
nasergamal <naser.gamal98@gmail.com>
2023-06-09 02:15:58 +0200

    4- changed inital check for arguments

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,10 +13,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *ptr = NULL;
 
+	if (!ht || !key || !(*key))
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 
-	if (!(ht->array[index]))
-		return (NULL);
 	ptr = ht->array[index];
 	while (ptr)
 	{
